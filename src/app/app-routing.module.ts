9fc3c3e01a1d95e59663144f17ddca310b9bd419
RMYanenko <yanenko.r.m@gmail.com>
2020-06-10 23:23:17 +0300

    create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {
+        path: '', redirectTo: '/', pathMatch: 'full'
+      },
+      {
+        path: '', component: HomePageComponent
+      },
+      {
+        path: 'user-satistics', component: UsersListComponent
+      },
+      {
+        path: 'user/:id', component: UserPageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
